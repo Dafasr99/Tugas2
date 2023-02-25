be00aa3c734f028a1cacff30ecb834ad3645588f
Dafasr99 <anandafa.syukur@ui.ac.id>
2023-02-26 00:49:44 +0700

    Tugas 2 PBP

--- study_tracker/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from study_tracker import Assignment
-
-class AssignmentForm(ModelForm):
-    class Meta:
-        model = Assignment
-        fields = ['name', 'type', 'subject', 'date', 'progress', 'description']
